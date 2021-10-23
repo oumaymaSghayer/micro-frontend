cc478d17ee7db6c4fc435f4de2ba195a5027d701
Oumayma Sghayer <extern.oumayma.sghayer@allianz.be>
2021-10-23 11:43:19 +0100

    add project

---
+++ projects/host/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'mfe1',
+    loadChildren: () =>
+      import('./../../../mfe1/src/app/mfefeature/mfefeature.module').then(
+        (m) => {
+          return m.MfefeatureModule;
+        }
+      ),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
